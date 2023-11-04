78858a537fe8967ff88b2692953fd9fe74dc113b
Cody Large <dlarge1@ewu.edu>
2023-11-03 20:56:30 -0700

    Good Progress

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,30 @@
-document.querySelector('.hamburger').addEventListener('click', () => {
-    document.querySelector('.nav-links').classList.toggle('expanded');
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
+
+  hamburger.addEventListener(
+    "click",
+    function () // Display nav links when hamburger is clicked
+    {
+      if (navLinks.style.display === "block") {
+        navLinks.style.display = "none";
+      } else {
+        navLinks.style.display = "block";
+      }
+    }
+  );
+
+  window.addEventListener(
+    "resize",
+    function () // resize in case user reverts size of window back to desktop
+    {
+      if (window.innerWidth > 600) {
+        navLinks.style.display = "flex";
+      } else {
+        if (navLinks.style.display === "flex") {
+          navLinks.style.display = "none";
+        }
+      }
+    }
+  );
+});
