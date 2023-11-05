90b73f6984a6418d99e6c43362fb7852ffecbdc5
Cody Large <dlarge1@ewu.edu>
2023-11-05 11:08:12 -0800

    img -> public
    hopefully fixed the images not showing up on netlify

--- src/scripts/blogPosts.js
+++ src/scripts/blogPosts.js
@@ -3,19 +3,19 @@ const posts = [
     title: "The Sun is Going To EXPLODE!?",
     href: "/posts/post-1/",
     date: "2023-11-01",
-    thumbnail: "/img/sun-exploding-2.webp",
+    thumbnail: "/public/sun-exploding-2.webp",
   },
   {
     title: "Blackholes",
     href: "/posts/post-2/",
     date: "2023-11-02",
-    thumbnail: "/img/blackhole.jpg",
+    thumbnail: "/public/blackhole.jpg",
   },
   {
     title: "Post 3",
     href: "/posts/post-3/",
     date: "2023-11-03",
-    thumbnail: "/img/sun-exploding.webp",
+    thumbnail: "/public/sun-exploding.webp",
   },
 ];
 
